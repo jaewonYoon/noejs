c450c3d494b791a1f43386ff78a3a7e8a8d5139f
jaewonYoon <21300492@handong.edu>
2019-10-18 21:22:17 +0900

    first touch

---
+++ 1/app.js
@@ -0,0 +1,14 @@
+const http= require('http');
+const express = require('express');
+
+const app = express();
+app.use((req,res,next) => {
+    console.log('In the middleware!');
+    next();
+});
+
+app.use((req,res,next) => {
+    console.log('In another middleware!');
+});
+
+app.listen(3002); 
