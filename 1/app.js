ac577fb5132ac38a622c3c1d19fa170079f19610
jaewonYoon <21300492@handong.edu>
2019-10-19 11:33:00 +0900

    second touch

--- 1/app.js
+++ 1/app.js
@@ -1,14 +1,23 @@
-const http= require('http');
+// path 
+const path = require('path'); 
+//express 모듈 사용 
 const express = require('express');
-
 const app = express();
-app.use((req,res,next) => {
-    console.log('In the middleware!');
-    next();
-});
+// router폴더의 라우터 가져오기
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use((req,res,next) => {
-    console.log('In another middleware!');
+// body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public'))); 
+
+
+app.use('/admin',adminRoutes); 
+app.use(shopRoutes); 
+
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 
 app.listen(3002); 
